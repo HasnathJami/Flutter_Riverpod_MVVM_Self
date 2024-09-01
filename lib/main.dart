ef15a38e92633508f34de6784b707d32d9ff1d14
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-09-02 01:43:16 +0600

    Counter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(
+    child: MaterialApp(
+      title: 'Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    ),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+final counterStateProvider = StateProvider<int>((ref) {
+  return 0;
+});
 
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column();
+    print('build');
+    return Scaffold(
+      body: Center(
+        child: CounterText(),
+      ),
+      floatingActionButton: Consumer(builder: (context, ref, child) {
+        return FloatingActionButton(
+          onPressed: () {
+            ref.read(counterStateProvider.notifier).state++;
+          },
+          child: Icon(Icons.add),
+        );
+      }),
+    );
+  }
+}
+
+class CounterText extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    print('CounterText build');
+    var value = ref.watch(counterStateProvider);
+    return Text(
+      'Value: $value',
+      style: Theme.of(context).textTheme.headlineMedium,
+    );
   }
 }
