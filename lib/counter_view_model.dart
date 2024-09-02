00b640cf77582f1c30fbac5193f9756c63824610
hasnath.chowdhury <hasnath.chowdhury@reddotdigitalit.com>
2024-09-02 11:37:10 +0600

    Another Optimal Approach with View Model

--- lib/counter_view_model.dart
+++ lib/counter_view_model.dart
@@ -3,24 +3,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class CounterViewModel extends StateNotifier<int> {
   CounterViewModel() : super(0);
 
+  //setter
   void incrementCounter() {
     state = state + 1;
   }
 
+  //setter
   void decrementCounter() {
     state--;
   }
 
+  //setter
   void resetCounter() {
     state = 0;
   }
 }
 
 // Define a StateNotifierProvider to expose the CounterViewModel
-final counterStateProvider =
-    StateNotifierProvider.autoDispose<CounterViewModel, int>((ref) {
-  return CounterViewModel();
-});
+final counterStateProvider = StateNotifierProvider.autoDispose<CounterViewModel, int>((ref) => CounterViewModel());
 
 // class CounterViewModel {
 //   final counterStateProvider = StateProvider.autoDispose<int>((ref) => 0);
--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,21 @@ void main() {
 
 // The main widget that displays the counter value and a button to increment it
 class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Access the ViewModel to call methods
+    final counterVM = ref.read(counterStateProvider.notifier);
+
     print('MyHomePage build');
 
-    // // Watch the counter state from the provider
+    // Watch the counter state from the provider
     // final counter = ref.watch(counterStateProvider);
 
     return Scaffold(
       body: Center(
-        child: Consumer(builder: (context, refValue, child){
+        child: Consumer(builder: (context, refValue, child) {
           // Watch the counter state from the provider
           final counter = refValue.watch(counterStateProvider);
           return Text(
@@ -37,7 +42,8 @@ class MyHomePage extends ConsumerWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Increment the counter by calling the method in the ViewModel
-          ref.read(counterStateProvider.notifier).incrementCounter();
+          // ref.read(counterStateProvider.notifier).incrementCounter();
+          counterVM.incrementCounter();
         },
         child: Icon(Icons.add),
       ),
